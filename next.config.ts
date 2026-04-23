6c70d9ca2c09fef0d2bcb71c8182e86081d1cc5a
Wasif <wasifahad300@gmail.com>
2026-04-23 21:02:07 +0600

    feat: add contact page and authorize Cloudinary domains in next config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cloudinary.com',
+      },
     ],
   },
 };
